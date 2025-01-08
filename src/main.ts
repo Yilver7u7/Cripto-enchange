287bdd49901ea1bf05e83b3101211bdd41ca880d
Yilver7u7 <yilversgg@gmail.com>
2025-01-07 20:02:07 -0500

    Update the consume API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+const appConfig = {
+  providers: [
+    provideHttpClient(),
+    // otros proveedores
+  ],
+};
 
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule)],
-}).catch((err) => console.error(err));
+
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
